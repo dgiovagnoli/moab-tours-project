695d76e5d7ad6d2af2890c2d80279b9c7631d1c2
dgiovagnoli <dgiovagnoli345@gmail.com>
2016-09-16 18:32:58 -0400

    Update SendEmail.php

--- src/email/SendEmail.php
+++ src/email/SendEmail.php
@@ -9,7 +9,8 @@ class SendEmail
         $from = getenv('MAIL_USERNAME');
 
 
-    $transport = \Swift_SmtpTransport::newInstance(getenv('MAIL_HOST'), getenv('MAIL_PORT'))
+    //$transport = \Swift_SmtpTransport::newInstance(getenv('MAIL_HOST'), getenv('MAIL_PORT'))
+    $transport = \Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
         ->setUsername(getenv('MAIL_USERNAME'))
         ->setPassword(getenv('MAIL_PASSWORD'));
 
