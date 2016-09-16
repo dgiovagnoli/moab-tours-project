8dd4a21350bfc2127584da8072ebce85646c1679
dgiovagnoli <dgiovagnoli345@gmail.com>
2016-09-16 17:17:10 -0400

    Update SendEmail.php

--- src/email/SendEmail.php
+++ src/email/SendEmail.php
@@ -2,9 +2,9 @@
 return [
     'driver' => env('MAIL_DRIVER', 'smtp'),
     'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
     'from' => ['address' => 'forgeddit.com@gmail.com', 'name' => 'forgeddit'],
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
     'username' => env('MAIL_USERNAME'),
     'password' => env('MAIL_PASSWORD'),
     'sendmail' => '/usr/sbin/sendmail -bs',
