34fb5d4219c13f9e9176a3b7ed50293f2a2d8d43
dgiovagnoli <dgiovagnoli345@gmail.com>
2016-09-16 12:55:49 -0400

    Update 20150822170817_seed_review_table.php

--- migrations/20150822170817_seed_review_table.php
+++ migrations/20150822170817_seed_review_table.php
@@ -7,7 +7,7 @@ class SeedReviewTable extends AbstractMigration
     public function up()
     {
         $this->execute("
-            insert into reviewss (title, review, user_id, created_at)
+            insert into reviews (title, review, user_id, created_at)
             values
             ('Review Title', 'Review text', 1, '2015-08-26 19:15:43')
         ");
